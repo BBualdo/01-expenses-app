be33859b8f72f3aa0d20f8326147175a50008304
Sebastian <mrseb10@gmail.com>
2023-09-28 08:30:00 +0200

    Used Card component as a wrapper (props.children)

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import Card from './Card';
 
 const Expenses = (props) => {
 	return (
-		<div className='expenses'>
+		<Card className='expenses'>
 			<ExpenseItem
 				title={props.title}
 				amount={props.amount}
 				date={props.date}
 			/>
-		</div>
+		</Card>
 	);
 };
 
