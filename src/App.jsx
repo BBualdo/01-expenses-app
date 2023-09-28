ee0f8eded377bd121028d9655a689a088a8d27e9
Sebastian <mrseb10@gmail.com>
2023-09-28 08:13:58 +0200

    Expense separated to new Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
 	const expenses = [
@@ -25,29 +26,20 @@ function App() {
 		},
 	];
 
+	const expenseElement = expenses.map((expense) => {
+		return (
+			<Expenses
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		);
+	});
+
 	return (
 		<div>
 			<h2>Let's get started!</h2>
-			<ExpenseItem
-				title={expenses[0].title}
-				amount={expenses[0].amount}
-				date={expenses[0].date}
-			/>
-			<ExpenseItem
-				title={expenses[1].title}
-				amount={expenses[1].amount}
-				date={expenses[1].date}
-			/>
-			<ExpenseItem
-				title={expenses[2].title}
-				amount={expenses[2].amount}
-				date={expenses[2].date}
-			/>
-			<ExpenseItem
-				title={expenses[3].title}
-				amount={expenses[3].amount}
-				date={expenses[3].date}
-			/>
+			{expenseElement}
 		</div>
 	);
 }
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = (props) => {
+	return (
+		<div className='expenses'>
+			<ExpenseItem
+				title={props.title}
+				amount={props.amount}
+				date={props.date}
+			/>
+		</div>
+	);
+};
+
+export default Expenses;
