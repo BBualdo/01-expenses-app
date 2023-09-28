c7d63b51365fb85990255654b2e32ba1f62f274c
Sebastian <mrseb10@gmail.com>
2023-09-28 08:43:50 +0200

    Orginising components

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 	return (
