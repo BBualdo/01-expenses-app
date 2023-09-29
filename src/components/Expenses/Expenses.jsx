8a39da56f21f9709625276e9f344474a3fd97e21
Sebastian <mrseb10@gmail.com>
2023-09-29 08:19:30 +0200

    Added pick Year handler and passed it to parent

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,16 +3,24 @@ import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+	const filterYear = (selectedYear) => {
+		console.log(selectedYear);
+	};
+
 	return (
-		<Card className='expenses'>
-			<ExpenseItem
-				title={props.title}
-				amount={props.amount}
-				date={props.date}
-			/>
-		</Card>
+		<div>
+			<ExpenseFilter onYearSelect={filterYear} />
+			<Card className='expenses'>
+				<ExpenseItem
+					title={props.title}
+					amount={props.amount}
+					date={props.date}
+				/>
+			</Card>
+		</div>
 	);
 };
 
