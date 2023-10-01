3829039db6e4ea24ffae645094660218461f9463
Sebastian <mrseb10@gmail.com>
2023-10-01 09:49:48 +0200

    Chart added

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState('2023');
@@ -20,6 +20,7 @@ const Expenses = (props) => {
 	return (
 		<Card className='expenses'>
 			<ExpenseFilter selected={filteredYear} onYearSelect={filterYear} />
+			<ExpensesChart expense={filteredExpenses} />
 			<ExpensesList items={filteredExpenses} />
 		</Card>
 	);
